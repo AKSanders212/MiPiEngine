0a803b495f281de5aae4c9235d5214596e088b83
AKSanders212 <aaronsanders6470@yahoo.com>
2020-11-14 08:07:37 -0800

    Reworked the engine into two screens: Main frame and gamplay window

--- MiPi.py
+++ MiPi.py
@@ -3,39 +3,60 @@
 
 import pygame
 from pygame import *
-
-# Gonna use tkinter for my gui module, but due to utilizing only one main instance at a time with pygame,
-# I am going to have to just use the pygame window as my main engine frame and reserve an area on the
-# frame for creating a game, then just launch a seperate pygame window for testing/playing a game.
-import tkinter as tk
-from tkinter import *
-
+import pygame_gui
+from pygame_gui import *
 import logging
 import datetime
-
 
 # Initialize pygame
 pygame.init()
 
-
-# All Global Variables
-GREEN = (0,255,0)
-LIGHT_BLUE = (173,216,230)
-WHITE = (255,255,255)
-LIGHTCOLOR = (170,170,170)
-DARKSHADE = (100,100,100)
+# --------------------------------- All Global Variables ----------------------------------------------------#
+# Color constants
+GREEN = (0, 255, 0)
+NOCOLOR = (0, 0, 0)
+LIGHT_BLUE = (173, 216, 230)
+WHITE = (255, 255, 255)
+DARKGRAY = (169, 169, 169)
+LIGHTCOLOR = (170, 170, 170)
+DARKSHADE = (100, 100, 100)
+# Fonts
 CorbelFont = pygame.font.SysFont('Corbel', 35)
 guitext = CorbelFont.render('File', True, WHITE)
+# Vertices, vertexes and sizes
 verts = [(50, 0), (100, 50), (50, 50), (0, 50)]
-size = (145, 145)
+size = (800, 625)
 
-# Creating the dimensions for the game engine screen
+# Creating the dimensions for the game play screen
 width = 800
 height = 625
 
+# Creating the dimensions for the main engine frame
+ewidth = 800
+eheight = 625
+
 # Datetime vars
 current_date = datetime.datetime.now()
-screen = pygame.display.set_mode((width, height))
+gamescreen = pygame.display.set_mode((width, height))
+enginescreen = pygame.display.set_mode((ewidth, eheight))
+
+# Main engine variables and constants
+mainframe = pygame_gui.UIManager((800, 625))
+sysclock = pygame.time.Clock()
+mainFPS = 60
+# Main engine frame settings
+engine_title = "MiPi Engine"
+pygame.display.set_caption(engine_title)
+# Game window settings
+game_title = "Gameplay Test"
+# Pygame_gui UI elements
+play_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((150, 150), (100, 50)),
+                                           text='Test Game', manager=mainframe)
+# Create this variable as global, else we get some cranky error!
+game_running = False
+
+
+# ---------------------------------------------------------------------------------------------------------#
 
 class MiPi:
     def __init__(self):
@@ -44,39 +65,47 @@ class MiPi:
     @staticmethod
     def EngineScreen():
 
-        # Pygame
-        title = "MiPi Engine"
-        pygame.display.set_caption(title)
-        screen.fill(LIGHT_BLUE)
+        delta_time = sysclock.tick(mainFPS) / 1000.0
 
-        # Loop used to keep the engine window running until the user closes it
-        running = True
+        engine_running = True
 
-        while (running):
+        while engine_running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if width/17 <= mouse[0] <= width/17+140 and height/17 <= mouse[1] <= height/17+40:
-                        print(current_date, "File button was clicked on submenu!")
+                    engine_running = False
 
-            mouse = pygame.mouse.get_pos()
+                enginescreen.fill(LIGHTCOLOR)
 
-            # If mouse is over a button it changes its shade color
-            if width/17 <= mouse[0] <= width/17+140 and height/17 <= mouse[1] <= height/17+40:
-                pygame.draw.rect(screen, LIGHTCOLOR, [width / 17, height / 17, 140, 40])
-            else:
-                pygame.draw.rect(screen, DARKSHADE, [width / 17, height / 17, 140, 40])
+                if event.type == pygame.USEREVENT:
+                    if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                        if event.ui_element == play_button:
+                            print(current_date, "Gameplay test window has been launched!")
+                            engine_running = False
+                            game_running = True
+                        else:
+                            print("Button not found!")
 
-            # Setting the text on the button
-            screen.blit(guitext, (width / 17 + 50, height / 17))
+                pygame.display.flip()
+                mainframe.process_events(event)
 
-            # Calling my DrawTriangle() method
-            MiPi.DrawTriangle()
+            mainframe.update(delta_time)
+            mainframe.draw_ui(enginescreen)
+            pygame.display.update()
 
-            # This is needed to display the window
-            pygame.display.flip()
+        while game_running:
+            for game in pygame.event.get():
+                if game.type == pygame.QUIT:
+                    game_running = False
 
+                enginescreen.fill(NOCOLOR)
+                gamescreen.fill(DARKGRAY)
+                mainframe.process_events(game)
+                pygame.display.set_caption(game_title)
+                MiPi.DrawTriangle()
+                pygame.display.flip()
+
+            pygame.display.update()
+            mainframe.update(delta_time)
 
     @staticmethod
     def EngineInit():
@@ -91,6 +120,5 @@ class MiPi:
         # Example code to draw a triangle
         triangle = pygame.Surface(size)
         pygame.draw.polygon(triangle, GREEN, verts)
-        screen.blit(triangle, (70, 90))
-
-
+        gamescreen.blit(triangle, (0, 0))
+        pygame.display.update()
